1971da3a0ce30d22f01d56a898899a9e6cf8cff1
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-13 13:13:21 -0400

    commit tree eac99e4b698c39d1cd5dbcc0c1aaf80d191c5614

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    let thankyouCards = []
+    for( let i = 0; i < namesArray.length; i++) {
+        thankyouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankyouCards
+}
+
+
+function countDown( x ) {
+    while ( x > 0 ) {
+        console.log( x );
+        x -= 1;
+    }
+    console.log(x);
+}
